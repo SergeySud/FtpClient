96d5a5ed2264d0f5a958537125dd0a171cd176e6
SergeySud <ln251705@gmail.com>
2022-09-12 07:33:22 +0300

    fixes

--- src/main/java/ClientMain.java
+++ src/main/java/ClientMain.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Main {
+public class ClientMain {
 
 
     public static void main(String[] args) throws IOException {
